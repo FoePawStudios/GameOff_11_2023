1d9fbdc3cb170520cf7ae73d9861203d981c1acb
FoePawStudios <FoePawStudios@users.noreply.github.com>
2023-11-07 13:42:58 -0500

    adding latest build and updating gitignore to allow latest build

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -32,54 +32,10 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    /*void FixedUpdate()
-    {
-        //Check for mouse-over on scalable objects using raycast
-        Vector2 mousePosV3 = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        foreach( GameObject scalableObject in GameObject.FindGameObjectsWithTag("Scalable") )
+        //Handle Movement Input
+        if (Input.GetKeyUp( KeyCode.Escape ) ) 
         {
-            if( scalableObject.GetComponent<Collider2D>().OverlapPoint( new Vector2(mousePosV3.x, mousePosV3.y) ) )
-            {
-                if (Input.mouseScrollDelta.y != 0)
-                {
-                    float scaleChange = (Input.mouseScrollDelta.y * Time.fixedDeltaTime * scaleRate);
-                    Vector3 startScale = scalableObject.transform.localScale;
-                    startScale.x = startScale.x + scaleChange;
-                    startScale.y = startScale.y + scaleChange;
-                    
-                    //If shrinking would cause object to be smaller than minimum, just set to minimum
-                    if(startScale.x < defaultMinScale)
-                    {
-                        float overshoot = defaultMinScale - startScale.x;
-                        startScale.x = startScale.x + overshoot;
-                        startScale.y = startScale.y + overshoot;
-                    }
-                    else if(startScale.x > defaultMaxScale)
-                    {
-                        float overshoot = startScale.x - defaultMaxScale;
-                        startScale.x = startScale.x - overshoot;
-                        startScale.y = startScale.y - overshoot;
-                    }
-
-                    if (startScale.y < defaultMinScale)
-                    {
-                        float overshoot = defaultMinScale - startScale.y;
-                        startScale.x = startScale.x + overshoot;
-                        startScale.y = startScale.y + overshoot;
-                    }
-                    else if (startScale.y > defaultMaxScale)
-                    {
-                        float overshoot = startScale.y - defaultMaxScale;
-                        startScale.x = startScale.x - overshoot;
-                        startScale.y = startScale.y - overshoot;
-                    }
-
-                    scalableObject.transform.localScale = startScale;
-                }
-            }
+            Application.Quit();
         }
-    }*/
+    }
 }
