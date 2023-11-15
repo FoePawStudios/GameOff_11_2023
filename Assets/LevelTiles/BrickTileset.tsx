b78a2b0698f1ba7fc7447ad0488a5c176062e888
FoePawStudios <FoePawStudios@users.noreply.github.com>
2023-11-14 19:17:29 -0500

    Added a new level to test tiling and jumping

---
+++ Assets/LevelTiles/BrickTileset.tsx
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="BrickTileset" tilewidth="16" tileheight="16" tilecount="25" columns="5">
+ <image source="BrickTileset.png" width="80" height="80"/>
+ <tile id="1">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+   <object id="2" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.43478" y="7.73913" width="0.0434783" height="0.0869565"/>
+   <object id="8" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="FullTileCollider" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="unity:SortingLayer" value="Background"/>
+  </properties>
+ </tile>
+</tileset>
